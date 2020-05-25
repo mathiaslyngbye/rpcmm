64518d3b20d51a83ffcfa95106e503339b584155
martinjakobsgaard <martin.jakobsgaard@gmail.com>
2020-05-25 17:49:47 +0200

    Basic system call for docker service launch

--- master/master.cpp
+++ master/master.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <stdlib.h>
 
 
 int main(int argc, char** argv)
 {
-    std::cout << "Hello friend" << std::endl;
+    std::string docker = "docker service create ";
+    std::string replicas = "--replicas 3";
+    std::string mounting = "--mount type=volume,dst=/mount/,volume-driver=local,volume-opt=type=nfs,\\\"volume-opt=o=nfsvers=4,addr=10.42.0.181\\\",volume-opt=device=:/clusterfs";
+    std::string dist = "alpine";
+    std::string command_container = "/bin/sh -c \"while true; do ls /mount/; sleep 2; done\"";
+
+    std::string command = docker+replicas+" "+mounting+" "+dist+" "+command_container;
+
+    std::cout << command << std::endl;
+    //system(command.c_str());
+
     return 0;
 }
